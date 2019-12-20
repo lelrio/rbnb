77ab928215dbc5733d5026032d00302b8772b450
leonard <leo151098@hotmail.fr>
2019-12-20 14:50:19 +0100

    quatrieme commit

--- screens/connexion.js
+++ screens/connexion.js
@@ -11,12 +11,12 @@ export default class loggedOut extends Component {
             password: '',
         };
     };
-    handleEmailChange = (evt) => {
-        this.setState({ email: evt.target.value });
+    handleEmailChange = (value) => {
+        this.setState({ email: value });
     }
       
-    handlePasswordChange = (evt) => {
-        this.setState({ password: evt.target.value });
+    handlePasswordChange = (value) => {
+        this.setState({ password: value });
     }   
     render() {
         const { email, password } = this.state;
@@ -36,10 +36,9 @@ export default class loggedOut extends Component {
                     </Text>
                     <TextInput
                         style={{ height: 40, width: 350 , borderBottomColor: '#77b5fe', borderBottomWidth: 1, marginLeft:22, color: 'white' }}
-                        onChangeText={text => console.log(text)}
                         type="text"
                         value={this.state.email}
-                        onChange={this.handleEmailChange}
+                        onChangeText={this.handleEmailChange}
                     />
                     <View style= {{flexDirection: "row",}}>
                         <Text style={{ color: 'white', fontSize:20, marginTop: 50, marginLeft:22}}>Mot de passe </Text>
@@ -47,10 +46,9 @@ export default class loggedOut extends Component {
                     </View>
                     <TextInput
                         style= {{ height: 40, width: 350, borderBottomColor: '#77b5fe',borderBottomWidth: 1, marginLeft:22, color: 'white' }}
-                        onChangeText= {text => console.log(text)}
                         secureTextEntry= {true}
                         value= {this.state.password}
-                        onChange={this.handlePasswordChange}
+                        onChangeText={this.handlePasswordChange}
                         type={password}
                     />
                 </View>
